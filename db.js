154b764197bc686c2b277189b3fca94eea8c87a3
jesus anez <accpanamerican71@gmail.com>
2025-04-06 12:40:49 -0400

    🔧 Corregido archivo db.js con conexión correcta a MongoDB Atlas

--- db.js
+++ db.js
@@ -3,4 +3,7 @@ const mongoose = require("mongoose");
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-});
+})
+.then(() => console.log("🟢 Conectado a MongoDB con Mongoose"))
+.catch((err) => console.error("🔴 Error conectando a MongoDB:", err));
+
