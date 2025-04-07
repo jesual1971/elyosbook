b11156e23b8809f24e73dbb09bc675cde91924e9
jesus anez <accpanamerican71@gmail.com>
2025-04-07 01:48:18 -0400

    Fix: usar process.env.MONGO_URI correctamente

--- db.js
+++ db.js
@@ -1,9 +1,11 @@
 const mongoose = require("mongoose");
 
-mongoose
-  .connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI, {
+  serverSelectionTimeoutMS: 10000,
+})
   .then(() => console.log("🟢 Conectado a MongoDB con Mongoose"))
   .catch((err) => console.error("🔴 Error conectando a MongoDB:", err));
+
 
 
 
