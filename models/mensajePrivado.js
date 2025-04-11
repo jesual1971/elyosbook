3c81315e8aaad648ef60a7c0bed803bd33dea77e
jesus anez <accpanamerican71@gmail.com>
2025-04-10 22:47:10 -0400

    ✅ Permitir mensajes sin texto (solo imagen)

--- models/mensajePrivado.js
+++ models/mensajePrivado.js
@@ -3,9 +3,11 @@ const mongoose = require("mongoose");
 const mensajePrivadoSchema = new mongoose.Schema({
   emisor: { type: String, required: true },
   receptor: { type: String, required: true },
-  contenido: { type: String, required: true },
+  contenido: { type: String, required: false }, // 👈 ahora no es obligatorio
+  imagen: { type: String }, // 👈 permite subir imagen
   fecha: { type: Date, default: Date.now },
-  leido: { type: Boolean, default: false } // ✅ nuevo campo
+  leido: { type: Boolean, default: false }
 });
+
 
 module.exports = mongoose.model("MensajePrivado", mensajePrivadoSchema);
